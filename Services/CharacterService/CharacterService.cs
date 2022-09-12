f25e77dddd706628047820697ebd7b08b63b33b2
Aman Tiwari <atiwari@pizoneinfotech.com>
2022-09-12 17:20:44 +0530

    se

---
+++ Services/CharacterService/CharacterService.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace project1.Controllers.Services.CharacterService
+{
+    public interface CharacterService
+    {
+        
+    }
+}
